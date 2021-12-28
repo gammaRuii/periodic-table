b816670765423785f20b2456caa448f280d38b48
rui <xruichen0411@gmail.com>
2021-12-27 17:52:42 -0800

    base framework

--- main.py
+++ main.py
@@ -19,22 +19,52 @@ class MainBody(GridLayout):
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
         self.cols = 18
+        self.rows = 7
         self.orientation = "lr-tb"
+        button_size = dp(103)
         with open("p.json") as p:
             e = p.read()
             el = json.loads(e)
             elements = el['elements']
-            button_size = dp(103)
-            b = Button(text = "H")
+            b = Button(text = "H", size_hint = (None,None), size = (button_size,button_size))
             self.add_widget(b)
-            # for i in range(16):
-            #     l = Label(text = "", size_hint = (None,None), size = (button_size,button_size))
-            #     self.add_widget(l)
-            # b = Button(text = "He", size_hint = (None,None), size = (button_size,button_size))
-            # self.add_widget(b)
-            # for i in range(2,4):
-            #     b = Button(text = "{}".format(elements[i]["symbol"], size_hint = (None,None), size = (button_size,button_size)))
-            #     self.add_widget(b)
+            for i in range(16):
+                l = Label(text = "", size_hint = (None,None), size = (button_size,button_size))
+                self.add_widget(l)
+            b = Button(text = "He", size_hint = (None,None), size = (button_size,button_size))
+            self.add_widget(b)
+            for i in range(2,4):
+                b = Button(text="{}".format(elements[i]["symbol"]), size_hint=(None, None), size=(button_size, button_size))
+                self.add_widget(b)
+            for i in range(10):
+                l = Label(text = "", size_hint = (None,None), size = (button_size,button_size))
+                self.add_widget(l)
+            for i in range(4,10):
+                b = Button(text="{}".format(elements[i]["symbol"]), size_hint=(None, None), size=(button_size, button_size))
+                self.add_widget(b)
+            for i in range(10,12):
+                b = Button(text="{}".format(elements[i]["symbol"]), size_hint=(None, None), size=(button_size, button_size))
+                self.add_widget(b)
+            for i in range(10):
+                l = Label(text = "", size_hint = (None,None), size = (button_size,button_size))
+                self.add_widget(l)
+            for i in range(12,18):
+                b = Button(text="{}".format(elements[i]["symbol"]), size_hint=(None, None), size=(button_size, button_size))
+                self.add_widget(b)
+            for i in range(18,56):
+                b = Button(text="{}".format(elements[i]["symbol"]), size_hint=(None, None), size=(button_size, button_size))
+                self.add_widget(b)
+            b = Button(text = "Lanthanides", size_hint=(None, None), size=(button_size, button_size))
+            self.add_widget(b)
+            for i in range(71,88):
+                b = Button(text="{}".format(elements[i]["symbol"]), size_hint=(None, None), size=(button_size, button_size))
+                self.add_widget(b)
+            b = Button(text = "Actinides", size_hint=(None, None), size=(button_size, button_size))
+            self.add_widget(b)
+            for i in range(103,118):
+                b = Button(text="{}".format(elements[i]["symbol"]), size_hint=(None, None), size=(button_size, button_size))
+                self.add_widget(b)
+
 class PeriodicTableApp(App):
     pass
 
